be780af5845d444125f3fc661d3cf0db2db556d9
Dmitriy <dmitriy.gayduk01@gmail.com>
2021-12-15 02:51:49 -0500

    draw the ship and give it movement

--- space-invaders.rkt
+++ space-invaders.rkt
@@ -5,6 +5,15 @@
 
 (define bg-color (color 25 25 112))
 
+(struct player (x y))
+
+(define my-ship (player (/ WIDTH 2) (* HEIGHT .9)))
+
+(define (draw-ship x y)
+  (triangle (+ x 30) y
+            x (- y 30)
+            (- x 30) y ))
+
 (define (setup)
   (size WIDTH HEIGHT)
   (background bg-color)
@@ -14,7 +23,14 @@
 
 
 (define (draw)
+  ;; controls
+  (when (and key-pressed (equal? key 'right))
+    (+= my-ship.x 6))
+  (when (and key-pressed (equal? key 'left))
+    (-= my-ship.x 6))
+
+  ;; draw
   (background bg-color)
-  
+  (draw-ship my-ship.x my-ship.y)
   ;(text (~a " Frame-rate: " frame-rate) 40 50)
   )
